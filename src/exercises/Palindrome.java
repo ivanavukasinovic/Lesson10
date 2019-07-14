9e9cb4ebdd5670812f059e679e01f3e890ba4f2a
Your Name <you@example.com>
2019-07-15 00:00:59 +0200

    Palindrome - code added

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,31 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import examples.FileHelper;
 
 public class Palindrome {
 
+	List<String> words = new ArrayList<String>();
+
+	public Palindrome() {
+		words = loadWords();
+	}
+
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	}
+
+	public boolean wordExists(String string) {
+		return words.contains(string);
+	}
+
+	public boolean isPalindrome(String string) {
+		StringBuilder builder = new StringBuilder(string);
+		if (wordExists(string) && string.equals(builder.reverse().toString())) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
