d1a59fcbcd811361d21777a16a02fe5967213cf6
Your Name <you@example.com>
2019-07-15 00:02:06 +0200

    PalindromeTest

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
+
 	Palindrome testPalindrome = new Palindrome();
 
 	@Test
 	void testLoadWords() {
 		assertTrue(testPalindrome.loadWords().contains("apple"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	@Test
+	void testWordExists() throws Exception {
+		assertTrue(testPalindrome.wordExists("banana"));
+		assertFalse(testPalindrome.wordExists("bananana"));
+	}
+
 	// 3. Test that a word is a palindrome
+	@Test
+	void testIsPalindrome() throws Exception {
+		assertTrue(testPalindrome.isPalindrome("level"));
+		assertFalse(testPalindrome.isPalindrome("aaaaaa"));
+		assertFalse(testPalindrome.isPalindrome("Aaron"));
+	}
 
 }
